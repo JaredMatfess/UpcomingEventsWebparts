7dcb2c2c2be18f683bb942a68e60528b6ac9ae72
JaredMatfess <jaredm@slalom.com>
2016-02-28 11:52:36 -0500

    First pass

---
+++ js/events.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var upcomingEventWP = upcomingEventWP || {};
+
+var test = "q";
+var test2 = 2; 
+
+upcomingEventWP.Title = "Here";
+upcomingEventWP.Title2 = "Here";
+upcomingEventWP.Title = "Here";
